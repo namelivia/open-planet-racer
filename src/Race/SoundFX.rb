0320364cb3a9656eb996f955c777ddd5013f45a1
namelivia <ohcan2@gmail.com>
2013-04-08 22:10:33 +0200

    game can be paused

---
+++ src/Race/SoundFX.rb
@@ -0,0 +1,22 @@
+class SoundFX
+
+def initialize(window,path)
+      @sound = Gosu::Sample.new(window,path)
+end
+def play(looping)
+    @playing = @sound.play(1.0,1,looping)
+end
+def setVolume(volume)
+  @playing.volume = volume
+end
+
+def pause()
+  @playing.pause()
+end
+
+def resume()
+  @playing.resume()
+end
+
+end
+

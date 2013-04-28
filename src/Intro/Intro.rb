c4414a9f17b3dbfdd452a45d4c88917891364aa7
namelivia <ohcan2@gmail.com>
2013-04-28 21:36:14 +0200

    Title Screen

--- src/Intro/Intro.rb
+++ src/Intro/Intro.rb
@@ -5,27 +5,30 @@ class Intro
   def initialize(window)
     @counter = 0
     @font = Gosu::Font.new(window, "Arial", 18)
+    @titleImage = Image.new(window,"../media/gfx/title.png",true)
     @finished = false;
   end
 
   def update(window)
     if window.button_down? Gosu::Button::KbEscape then
-       @counter = 500
+       @counter = 1000
     else
        @counter += 1
     end
-    if @counter == 500
+    if @counter == 1000
        @finished = true
     end
   end
 
   def draw(window)
-    color = Color.new(0,0,0,255)
-    window.draw_quad(0,0,color,0,SCREEN_HEIGHT,color,SCREEN_WIDTH,0,color,SCREEN_WIDTH,SCREEN_HEIGHT,color)
-    @font.draw("Open Planet Racer", 10, 10, 1.0, 1.0, 1.0)
-    @font.draw("By Namelivia", 10, 30, 1.0, 1.0, 1.0)
-    @font.draw("http://www.github.com/namelivia", 10, 50, 1.0, 1.0, 1.0)
-    @font.draw("This is just a work in progress demo!", 10, 70, 1.0, 1.0, 1.0)
+    if @counter < 500
+      color = Color.new(0,0,0,255)
+      window.draw_quad(0,0,color,0,SCREEN_HEIGHT,color,SCREEN_WIDTH,0,color,SCREEN_WIDTH,SCREEN_HEIGHT,color)
+      @font.draw("http://www.github.com/namelivia", 10, 50, 1.0, 1.0, 1.0)
+      @font.draw("This is just a work in progress demo!", 10, 70, 1.0, 1.0, 1.0)
+    else
+      @titleImage.draw(0,0,0)
+    end
   end
 end
 
